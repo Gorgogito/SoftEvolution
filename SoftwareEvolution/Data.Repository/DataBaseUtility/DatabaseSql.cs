14a1f00e11d4bf06d48b78785acd4c67886fdef8
Jorge Torres <ta_jJorge_luis@hotmail.com>
2022-01-18 09:45:12 -0500

    Actualización BackEnd

--- SoftwareEvolution/Data.Repository/DataBaseUtility/DatabaseSql.cs
+++ SoftwareEvolution/Data.Repository/DataBaseUtility/DatabaseSql.cs
@@ -6,300 +6,300 @@ using System.Data.SqlClient;
 
 namespace Infraestructura.Repository
 {
-    public class DatabaseSql : IDatabase
+  public class DatabaseSql : IDatabase
+  {
+    private DbConnection connection;
+    private DbCommand command;
+    private DbProviderFactory factory;
+    private DbTransaction transaction;
+    private int commandtimeout;
+
+    public DatabaseSql()
     {
-        private DbConnection connection;
-        private DbCommand command;
-        private DbProviderFactory factory;
-        private DbTransaction transaction;
-        private int commandtimeout;
-
-        public DatabaseSql()
-        {
-            var providerFactory = DatabaseHelper.DbProvider() ?? "";
-            DbProviderFactories.RegisterFactory(providerFactory, SqlClientFactory.Instance);
-            factory = DbProviderFactories.GetFactory(providerFactory);
-        }
-
-        public void Open()
-        {
-            connection = factory.CreateConnection();
-            connection.ConnectionString = DatabaseHelper.DbConnectionString();
-            try
-            {
-                if (connection != null && connection.State != ConnectionState.Open)
-                    connection.Open();
-            }
-            catch (Exception ex)
-            {
-                Log.GetLogger(typeof(DatabaseSql)).Error(System.Reflection.MethodBase.GetCurrentMethod().Name, ex);
-                throw;
-            }
-        }
-
-        public void OpenTransaction()
-        {
-            connection = factory.CreateConnection();
-            connection.ConnectionString = DatabaseHelper.DbConnectionString();
-            try
-            {
-                if (connection != null && connection.State != ConnectionState.Open)
-                {
-                    connection.Open();
-                    if (transaction != null)
-                        transaction = connection.BeginTransaction();
-                }
-
-            }
-            catch (Exception ex)
-            {
-                Log.GetLogger(typeof(DatabaseSql)).Error(System.Reflection.MethodBase.GetCurrentMethod().Name, ex);
-                throw;
-            }
-        }
-
-        public void Close()
-        {
-            try
-            {
-                if (connection != null && connection.State != ConnectionState.Closed)
-                    connection.Close();
-            }
-            catch (Exception ex)
-            {
-                Log.GetLogger(typeof(DatabaseSql)).Error(System.Reflection.MethodBase.GetCurrentMethod().Name, ex);
-                throw;
-            }
-        }
-
-        public string CommandText
-        {
-            set
-            {
-                if (command == null)
-                {
-                    command = factory.CreateCommand();
-
-                }
-                if (connection == null)
-                {
-                    this.Open();
-                }
-                command.Connection = connection;
-                command.CommandType = CommandType.Text;
-                command.CommandText = value;
-            }
-        }
-
-        public string ProcedureName
-        {
-            set
-            {
-                if (command == null)
-                {
-                    command = factory.CreateCommand();
-                }
-                if (connection == null)
-                {
-                    this.Open();
-                }
-                command.Connection = connection;
-                command.CommandType = CommandType.StoredProcedure;
-                command.CommandText = value;
-
-                if (Int32.TryParse("60", out commandtimeout))
-                {
-                    command.CommandTimeout = commandtimeout;
-                }
-
-            }
-        }
-
-        public void CommandTimeout(int timeOut = -1)
-        {
-            if (timeOut > 0)
-                command.CommandTimeout = timeOut;
-            else
-            {
-                if (Int32.TryParse("60", out commandtimeout))
-                {
-                    command.CommandTimeout = commandtimeout;
-                }
-            }
-        }
-
-        public object GetParameter(int index)
-        {
-            if (command != null)
-            {
-                try { return command.Parameters[index].Value; }
-                catch { return null; }
-            }
-            return null;
-        }
-
-        public object GetParameter(string name)
-        {
-            if (command != null)
-            {
-                try { return command.Parameters[name].Value; }
-                catch { return null; }
-            }
-            return null;
-        }
-
-        public void AddParameter(string parameterName, DbType parameterType, ParameterDirection parameterDirection, object parameterValue)
-        {
-            if (command != null)
-            {
-                DbParameter parameter = factory.CreateParameter();
-                parameter.ParameterName = parameterName;
-                parameter.DbType = parameterType;
-                parameter.Direction = parameterDirection;
-                parameter.Value = parameterValue ?? DBNull.Value;
-                parameter.SourceColumnNullMapping = true;
-                if (parameterType == DbType.String && (parameterDirection == ParameterDirection.Output || parameterDirection == ParameterDirection.InputOutput)) parameter.Size = -1;
-                command.Parameters.Add(parameter);
-            }
-        }
-
-        public void AddParameter(string parameterName, object parameterType, ParameterDirection parameterDirection, object parameterValue)
-        {
-            if (command != null)
-            {
-                DbParameter parameter = factory.CreateParameter();
-                parameter.ParameterName = parameterName;
-                parameter.DbType = (DbType)parameterType;
-                parameter.Direction = parameterDirection;
-                parameter.Value = parameterValue ?? DBNull.Value;
-                parameter.SourceColumnNullMapping = true;
-                command.Parameters.Add(parameter);
-            }
-        }
-        public void AddParameterSQL(SqlParameter SQLparameter)
-        {
-            if (command != null)
-            {
-                command.Parameters.Add(SQLparameter);
-            }
-        }
-
-
-        public void AddTransaction()
-        {
-            try
-            {
-                if (transaction != null)
-                {
-                    if (command != null)
-                    {
-                        command.Transaction = transaction;
-                    }
-
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.GetLogger(typeof(DatabaseSql)).Error(System.Reflection.MethodBase.GetCurrentMethod().Name, ex);
-                throw;
-            }
-        }
-
-        public void CommitTransaction()
-        {
-            this.transaction.Commit();
-        }
-
-        public void RollBackTransaction()
-        {
-            this.transaction.Rollback();
-        }
-
-        public IDataReader GetDataReader(bool close = true)
-        {
-
-            try
-            {
-                if (close)
-                {
-                    return command.ExecuteReader(CommandBehavior.SequentialAccess | CommandBehavior.CloseConnection);
-                }
-                else
-                {
-                    return command.ExecuteReader(CommandBehavior.SequentialAccess);
-                }
-            }
-            catch
-            {
-
-                throw;
-            }
-        }
-
-        public int Execute()
-        {
-            return command.ExecuteNonQuery();
-        }
-
-        public object ExecuteScalar()
-        {
-            return command.ExecuteScalar();
-        }
-
-        public DataTable GetDataTable()
-        {
-            DataTable dt = new DataTable();
-
-            using (DbDataAdapter da = factory.CreateDataAdapter())
-            {
-                da.SelectCommand = command;
-                da.Fill(dt);
-            }
-
-            return dt;
-        }
-
-        public void ClearParamaters()
-        {
-            if (command != null)
-            {
-                command.Parameters.Clear();
-            }
-        }
-
-        ~DatabaseSql()
-        {
-            Dispose(false);
-        }
-
-        #region IDisposable Members
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                // free managed resources
-                if (connection != null)
-                {
-                    connection.Dispose();
-                    connection = null;
-                }
-                if (command != null)
-                {
-                    command.Dispose();
-                    command = null;
-                }
-                factory = null;
-            }
-        }
-
-     
-        #endregion
+      var providerFactory = DatabaseHelper.DbProvider() ?? "";
+      DbProviderFactories.RegisterFactory(providerFactory, SqlClientFactory.Instance);
+      factory = DbProviderFactories.GetFactory(providerFactory);
     }
+
+    public void Open()
+    {
+      connection = factory.CreateConnection();
+      connection.ConnectionString = DatabaseHelper.DbConnectionString();
+      try
+      {
+        if (connection != null && connection.State != ConnectionState.Open)
+          connection.Open();
+      }
+      catch (Exception ex)
+      {
+        Log.GetLogger(typeof(DatabaseSql)).Error(System.Reflection.MethodBase.GetCurrentMethod().Name, ex);
+        throw;
+      }
+    }
+
+    public void OpenTransaction()
+    {
+      connection = factory.CreateConnection();
+      connection.ConnectionString = DatabaseHelper.DbConnectionString();
+      try
+      {
+        if (connection != null && connection.State != ConnectionState.Open)
+        {
+          connection.Open();
+          if (transaction != null)
+            transaction = connection.BeginTransaction();
+        }
+
+      }
+      catch (Exception ex)
+      {
+        Log.GetLogger(typeof(DatabaseSql)).Error(System.Reflection.MethodBase.GetCurrentMethod().Name, ex);
+        throw;
+      }
+    }
+
+    public void Close()
+    {
+      try
+      {
+        if (connection != null && connection.State != ConnectionState.Closed)
+          connection.Close();
+      }
+      catch (Exception ex)
+      {
+        Log.GetLogger(typeof(DatabaseSql)).Error(System.Reflection.MethodBase.GetCurrentMethod().Name, ex);
+        throw;
+      }
+    }
+
+    public string CommandText
+    {
+      set
+      {
+        if (command == null)
+        {
+          command = factory.CreateCommand();
+
+        }
+        if (connection == null)
+        {
+          this.Open();
+        }
+        command.Connection = connection;
+        command.CommandType = CommandType.Text;
+        command.CommandText = value;
+      }
+    }
+
+    public string ProcedureName
+    {
+      set
+      {
+        if (command == null)
+        {
+          command = factory.CreateCommand();
+        }
+        if (connection == null)
+        {
+          this.Open();
+        }
+        command.Connection = connection;
+        command.CommandType = CommandType.StoredProcedure;
+        command.CommandText = value;
+
+        if (Int32.TryParse("60", out commandtimeout))
+        {
+          command.CommandTimeout = commandtimeout;
+        }
+
+      }
+    }
+
+    public void CommandTimeout(int timeOut = -1)
+    {
+      if (timeOut > 0)
+        command.CommandTimeout = timeOut;
+      else
+      {
+        if (Int32.TryParse("60", out commandtimeout))
+        {
+          command.CommandTimeout = commandtimeout;
+        }
+      }
+    }
+
+    public object GetParameter(int index)
+    {
+      if (command != null)
+      {
+        try { return command.Parameters[index].Value; }
+        catch { return null; }
+      }
+      return null;
+    }
+
+    public object GetParameter(string name)
+    {
+      if (command != null)
+      {
+        try { return command.Parameters[name].Value; }
+        catch { return null; }
+      }
+      return null;
+    }
+
+    public void AddParameter(string parameterName, DbType parameterType, ParameterDirection parameterDirection, object parameterValue)
+    {
+      if (command != null)
+      {
+        DbParameter parameter = factory.CreateParameter();
+        parameter.ParameterName = parameterName;
+        parameter.DbType = parameterType;
+        parameter.Direction = parameterDirection;
+        parameter.Value = parameterValue ?? DBNull.Value;
+        parameter.SourceColumnNullMapping = true;
+        if (parameterType == DbType.String && (parameterDirection == ParameterDirection.Output || parameterDirection == ParameterDirection.InputOutput)) parameter.Size = -1;
+        command.Parameters.Add(parameter);
+      }
+    }
+
+    public void AddParameter(string parameterName, object parameterType, ParameterDirection parameterDirection, object parameterValue)
+    {
+      if (command != null)
+      {
+        DbParameter parameter = factory.CreateParameter();
+        parameter.ParameterName = parameterName;
+        parameter.DbType = (DbType)parameterType;
+        parameter.Direction = parameterDirection;
+        parameter.Value = parameterValue ?? DBNull.Value;
+        parameter.SourceColumnNullMapping = true;
+        command.Parameters.Add(parameter);
+      }
+    }
+    public void AddParameterSQL(SqlParameter SQLparameter)
+    {
+      if (command != null)
+      {
+        command.Parameters.Add(SQLparameter);
+      }
+    }
+
+
+    public void AddTransaction()
+    {
+      try
+      {
+        if (transaction != null)
+        {
+          if (command != null)
+          {
+            command.Transaction = transaction;
+          }
+
+        }
+      }
+      catch (Exception ex)
+      {
+        Log.GetLogger(typeof(DatabaseSql)).Error(System.Reflection.MethodBase.GetCurrentMethod().Name, ex);
+        throw;
+      }
+    }
+
+    public void CommitTransaction()
+    {
+      this.transaction.Commit();
+    }
+
+    public void RollBackTransaction()
+    {
+      this.transaction.Rollback();
+    }
+
+    public IDataReader GetDataReader(bool close = true)
+    {
+
+      try
+      {
+        if (close)
+        {
+          return command.ExecuteReader(CommandBehavior.SequentialAccess | CommandBehavior.CloseConnection);
+        }
+        else
+        {
+          return command.ExecuteReader(CommandBehavior.SequentialAccess);
+        }
+      }
+      catch
+      {
+
+        throw;
+      }
+    }
+
+    public int Execute()
+    {
+      return command.ExecuteNonQuery();
+    }
+
+    public object ExecuteScalar()
+    {
+      return command.ExecuteScalar();
+    }
+
+    public DataTable GetDataTable()
+    {
+      DataTable dt = new DataTable();
+
+      using (DbDataAdapter da = factory.CreateDataAdapter())
+      {
+        da.SelectCommand = command;
+        da.Fill(dt);
+      }
+
+      return dt;
+    }
+
+    public void ClearParamaters()
+    {
+      if (command != null)
+      {
+        command.Parameters.Clear();
+      }
+    }
+
+    ~DatabaseSql()
+    {
+      Dispose(false);
+    }
+
+    #region IDisposable Members
+
+    public void Dispose()
+    {
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+      if (disposing)
+      {
+        // free managed resources
+        if (connection != null)
+        {
+          connection.Dispose();
+          connection = null;
+        }
+        if (command != null)
+        {
+          command.Dispose();
+          command = null;
+        }
+        factory = null;
+      }
+    }
+
+
+    #endregion
+  }
 }
